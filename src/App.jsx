bba14c2c0f77965727092cbe657fcc07cd77a876
uuronn <uuronprogram@gmail.com>
2022-01-16 06:21:39 +0900

    ボタン制限

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 
 export const App = () => {
-  const time = 10
+  const time = 5
   const [timer, setTimer] = useState(time)
+  const [disabled, setDisabled] = useState(false)
 
   const onClickStart = () => {
+    setDisabled({disabled: true});
     setTimeout(() => {
       setTimer(prevState => prevState - 1)
     }, 1000)
   }
-
+  
   useEffect(() => {
     if(timer === time) return;
     if(timer === 0) return;
@@ -22,7 +24,7 @@ export const App = () => {
       <main>
         <div>
           <p>{timer}</p>
-          <button onClick={onClickStart}>スタート</button>
+          <button disabled={disabled} onClick={onClickStart}>スタート</button>
         </div>
         <div>キャラクターコンポーネント</div>
         <div>オプションコンポーネント</div>
